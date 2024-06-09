c73ec8389f016a00ad6b0e8f23e5364cb90515eb
Segun <sogbesansegun22@gmail.com>
2024-06-09 16:27:31 +0100

    Feat: load file name into pinecone

--- src/lib/pinecone/client.ts
+++ src/lib/pinecone/client.ts
@@ -1,4 +1,6 @@
 import { Pinecone } from "@pinecone-database/pinecone"
+import { downloadFromS3 } from "../s3/server"
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf"
 
 export const getPineconeClient = () => {
   return new Pinecone({
@@ -6,4 +8,14 @@ export const getPineconeClient = () => {
   })
 }
 
-export async function loadS3IntoPinecone(file_key: string) {}
+export async function loadS3IntoPinecone(file_key: string) {
+  console.log("downloading s3 into file system")
+
+  const file_name = await downloadFromS3(file_key)
+  if (!file_name) {
+    throw new Error("could not download from s3")
+  }
+  console.log("loading pdf into memory" + file_name)
+  const loader = new PDFLoader(file_name)
+  const pages = await loader.load()
+}
---
+++ src/lib/s3/server.ts
@@ -0,0 +1,3 @@
+export const downloadFromS3 = async (file_key: string): Promise<string> => {
+  return new Promise(async (resolve, reject) => {})
+}
