246e7a673860cc06ae53b9373f5624649cf267a3
Segun <sogbesansegun22@gmail.com>
2024-06-11 00:02:34 +0100

    Feat: a whole lot of cleanup

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,15 +1,15 @@
-import { convertPdfToDocuments, generateNotes, loadPdfFromUrl } from "@/lib"
+// import { convertPdfToDocuments, generateNotes, loadPdfFromUrl } from "@/lib"
 import { NextResponse } from "next/server"
 
 export async function POST(req: Request) {
   try {
-    const { paperUrl } = await req.json()
-    let pdfAsBuffer = await loadPdfFromUrl(paperUrl)
+    // const { paperUrl } = await req.json()
+    // let pdfAsBuffer = await loadPdfFromUrl(paperUrl)
 
-    const documents = await convertPdfToDocuments(pdfAsBuffer)
-    const notes = await generateNotes(documents)
+    // const documents = await convertPdfToDocuments(pdfAsBuffer)
+    // const notes = await generateNotes(documents)
 
-    return NextResponse.json(notes)
+    return NextResponse.json(null)
   } catch (error) {
     console.error(error, "Error==============")
   }
