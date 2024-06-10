561d4b86a6849ce635e255294c8e1807a43c3303
Segun <sogbesansegun22@gmail.com>
2024-06-10 11:51:55 +0100

    Fix: deployment fix

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -2,8 +2,9 @@ import {
   generateUploadButton,
   generateUploadDropzone
 } from "@uploadthing/react"
+import { OurFileRouter } from "../server/uploadthing"
 
-import type { OurFileRouter } from "@/app/api/uploadthing"
+// import type { OurFileRouter } from "@/app/api/uploadthing"
 
 export const UploadButton = generateUploadButton<OurFileRouter>()
 export const UploadDropzone = generateUploadDropzone<OurFileRouter>()
