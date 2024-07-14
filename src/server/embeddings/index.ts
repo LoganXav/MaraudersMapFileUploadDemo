539c2d48b9875ad53f856f4f1deec36dbfe070fb
Segun <sogbesansegun22@gmail.com>
2024-07-14 22:45:47 +0100

    Fix: pinecone prod fix

--- src/server/embeddings/index.ts
+++ src/server/embeddings/index.ts
@@ -17,23 +17,18 @@ export async function getEmbeddings(text: string) {
 }
 
 export async function getContext(query: string, fileKey: string) {
-  console.log(query, "query ================");
   const queryEmbeddings = await getEmbeddings(query);
-  console.log(queryEmbeddings, "queryEmbeddings ================");
-  const matches = await getMatchesFromEmbeddings(queryEmbeddings, fileKey);
 
-  console.log(matches, "matches ================");
+  const matches = await getMatchesFromEmbeddings(queryEmbeddings, fileKey);
 
   const qualifyingDocs = matches.filter((match) => match.score && match.score > 0.7);
 
-  console.log(qualifyingDocs, "qualifyingDocs ================");
   type Metadata = {
     text: string;
     pageNumber: number;
   };
 
   let docs = qualifyingDocs.map((match) => (match.metadata as Metadata).text);
-  console.log(docs, "docs ================");
   // 5 vectors
   return docs.join("\n").substring(0, 3000);
 }
